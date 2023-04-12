7b15f1268dcaade453a9f809a8bd319b4cd29a5d
Sébastien Barbieri <sebastien.barbieri@gmail.com>
2023-04-12 23:46:14 +0200

    intial commit

---
+++ src/App.svelte
@@ -0,0 +1,174 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import Counter from './components/Counter.svelte'
+  import Section from './components/Section.svelte'
+</script>
+
+<main>
+  <article>
+    <h1>Quelques mots sur notre hackathon</h1>
+    <Section hr>
+      <h2>L'idée</h2>
+      <p>Il faut remonter deux ans en arrière pour voir les prémices de
+      ce hackathon.</p>
+      <p>A l'époque à la RTBF on commençait à réaliser des articles
+      « long format » des articles de fond avec beaucoup de
+      contenu, d'images et on essayait d'y intégrer des éléments
+      interactifs.</p>
+      <p>Par le passé nous en avions fait quelques uns avec l'équipe
+      UX, les webmasters et les développeurs, mais un article qui prends
+      une semaine aux équipe techniques c'était un petit peu lourd.</p>
+      <p>Donc les journalistes avaient commencé à s'intéresser et à
+      se former en ligne à L'HTML d'abord et également au traitement
+      des données. L'objectif était d'être autonome. Ensuite ils en
+      sont rapidement arrivé à devoir coder en python pour traiter les
+      données et en javascript pour l'interface.</p>
+      <p>Avec les limitations imparties par le site sur lequel ils
+      déposaient leurs articles ils ont du trouver de plus en plus
+      d'astuce pour faire fonctionner ce qu'ils arrivaient à faire
+      chez eux sur le site de la RTBF.</p>
+      <p>On en était là et c'est ici qu'en discutant avec les
+      journalistes et en analysant la presse anglo-saxonne nous nous sommes
+      dit qu'un nouveau type de journaliste était nécessaire. Une sorte
+      de data journaliste capable de tout faire.</p>
+      <p>Est-ce que ça existe ? Est-ce que les écoles forment à ce
+      type de profile ?</p>
+      <p>Peut-être que ce serait bien de construire sur nos acquis une
+      master classe et de la partager avec les facultés de journalisme
+      afin d'avoir ce genre de profils sur le marché d'ici 2 à 5 ans.</p>
+      <p>Évidemment nous nous trompions pas mal. L'idée était
+      séduisante mais l'application impossible.</p>  
+    </Section>
+    <h2>La gestation</h2>
+    <p>Pour un projet comme celui-là avec beaucoup de partenaire,
+    c'était indispensable d'avoir un support du plus haut niveau.
+    Alain Dremière qui était en charge de l'équipe de rédaction
+    info web et François Jadoule, responsable recherche et développement
+    côté contenu, m'accompagnèrent chez l'administrateur général
+    de la RTBF afin de présenter l'idée.</p>
+    <p>Nous avons directement eu son support et ils nous a soutenu dans
+    notre démarche auprès de différentes facultés.</p>
+    <p>C'est à ce moment qu'on s'est rendu compte de la complexité
+    de mettre en place une master class entrant dans le cadre d'un
+    programme universitaire. Surtout en cours d'année.</p>
+    <p>Intégrer un programme ça se prépare deux ans à l'avance. Il
+    faut une base solide et les universités sont souvent d'une manière
+    ou d'une autre en compétition. Alors toutes les rassembler autour
+    de la table et intégrer un programme commun, en même temps, juste
+    pour une semaine, c'est compliqué.</p>
+    <p>Et surtout ce programme allait au-delà du Journalisme, en effet
+    il s'adresse à tous les acteur de la communication mais également
+    aux ingénieurs et informaticiens, aux étudiants en économie ou en
+    marketing, il peut également toucher les ergonomes et les équipes
+    UX, il était vachement plus large que ce que nous imaginions.</p>
+    <p>De « Summer Class » on est passé à « Winter
+    Class » puis à nouveau à « Spring Class » puis
+    « Summer Class »… on tournait en rond. Sans aboutir. En
+    imaginant quelque chose qui a de plus en plus d'impact et qui ne se
+    limite même plus aux université mes également aux hautes écoles.</p>
+    <p>Le projet restait un doux rêve, de plus en plus complexe.</p>
+    <p>Entre temps le recrutement avançait à la RTBF et une cellule
+    Decrypt était en train de voir le jour.</p>
+    <p>On se rendait compte également qu'il fallait des compétences
+    techniques et ergonomique directement au sein de la rédaction, déjà
+    en interne les mentalités évoluaient. On s'orientait vers une
+    équipe mutli-disciplinaire.</p>
+    <h2>Le coup d'accélérateur</h2>
+    <p>C'était important de ne plus porter le projet tout seul et de
+    partir chercher de l'aide.</p>
+    <p>Une première rencontre avec Damien Van Achter qui accepte de
+    monter à bord du projet et d'en devenir l'orchestrateur permet
+    de remettre en perspective certains éléments à commencer par le
+    format. Passer de master class en hackathon.</p>
+    <p>Une seconde rencontre avec Karim Douieb me permet de comprendre un
+    message fort lié et un talent présent chez tous les ingénieurs,
+    informaticiens, mathématicien, statisticien : la capacité de
+    faire parler des données très complexe, de les rendre digestes,
+    amusantes parfois, enrichissante certainement et surtout, toujours,
+    les garder les plus objectives possible.</p>
+    <p>Une troisième rencontre grâce à Corinne Boulangier avec la
+    Haute école Albert Jacquard (HEAJ) via Isabelle Sacré et nous
+    avions une haute école qui acceptait de tester le concept avec nous
+    et surtout un lieu : Namur et une date : le 15  avril
+    ou le 21 avril. 
+    </p>
+    <p>En trois rencontre tout ce concrétise.</p>
+    <h2>La préparation</h2>
+    <p>Karim Douieb accepte d'être notre premier speaker, nous
+    conseil, nous explique certains points.</p>
+    <p>On crée la réunion en ligne, faute de pouvoir faire autrement,
+    et on valide un programme qu'on avait préparé rapidement avec
+    Damien Van Achter. On l'adapte on le simplifie.</p>
+    <p>Autour de la table nous avions avec nous l'UCLouvain représenté
+    par Antonin Descampe, mais aussi grâce aux conseils de l'un et de
+    l'autre Becode !</p>
+    <p>On était parti du milieu universitaire d'une seule faculté et
+    on se retrouvait avec un modèle qui intègre l'ensemble des types
+    de formations supérieures : les universités, les hautes écoles et
+    la formation alternative.</p>
+    <p>Ceci me touche particulièrement, étant moi-même quelqu'un qui
+    a parcouru tous ces types d'enseignements et qui crois en la
+    formation continue et permanente dans des domaines divers. Qu'importe
+    la finalité, l'expérience et le chemin en lui même est
+    enrichissant.</p>
+    <p>Ensuite il faut trouver les thématiques, les données, les
+    speakers, les coaches, les locaux, le logement et la nourriture.</p>
+    <p>Tout le monde suit, lorsque je vais voir les gens, ils acceptent
+    de venir, lorsque Damien contacte son réseau, les gens suivent.</p>
+    <h2>L'équipe</h2>
+    <p>La HEAJ en premier nous propose une équipe composée d'un
+    professeur et chef de projet dans les personnes de Simon Marchal et
+    Bruno Wagener.</p>
+    <p>A L'UCLouvain Antonin nous propose Lavinia Rotili et lui même.</p>
+    <p>A BeCode Pierre Warnier et Louis de Viron sont super enthousiastes
+    et peuvent même s'occuper de préparer certaines données.</p>
+    <p>Chez Bouké.Média ils proposent leurs locaux et le rédacteur en
+    chef, Grégory Anciaux, accepte de faire partie du jury et de
+    coacher.</p>
+    <p>A la RTBF Xavier Lambert et Ambroise Carton acceptent de soutenir
+    le hackathon en offrant de faire partie du jury et surtout en
+    acceptant de publier (après re-travail) les articles sélectionnés
+    en mentionnant les noms des participants.</p>
+    <p>Yann Guégan accepte d'être notre deuxième speaker.</p>
+    <p>Philippe Deltenre sera notre speaker final pour préparer la
+    partie pitch.</p>
+    <p>Enfin Delphine Jenart accepte de venir coacher les équipes le
+    samedi matin.</p>
+    <p>Enfin Corinne Boulangier et Damien Van Achter gèrent l'avant,
+    le pendant et l'après.</p>
+    <h2>Les participants</h2>
+    <p>Et puis tout ça ne serait rien sans vous.</p>
+    <p>Je ne vous raconte pas la joie de découvrir d'abord 2-3
+    inscrits, puis 4, puis 7, … puis 28!</p>
+    <p>Quel plaisir ! Quel engouement, les messages que je reçois sont
+    sympathiques, constructifs.</p>
+    <p>Quel diversité de profils.</p>
+    <p>Tant en terme de genre.</p>
+    <p>Que de formation.</p>
+    <p>Des profils complémentaire pour créer des équipes qui sont plus
+    fortes ensemble que la sommes des individualités prisent séparément.</p>
+    <p>Pour créer des rencontre et peut-être des liens.</p>
+    <p>Pour voir les choses différemment, prendre du recul.</p>
+    <p>Et puis qui sait vous créer un chemin de vie, vous offrir des
+    opportunités alternatives.</p>
+  </article>
+</main>
+
+<style>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .read-the-docs {
+    color: #888;
+  }
+</style>
